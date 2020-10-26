055dd6785db25448b77ff417d5ac742fa12f6fb9
Garrett <gnjulaka@gmail.com>
2020-10-25 19:33:45 -0500

    Just set the new function's input on the function your'e setting it to. As in blah.method = function(input) { etc }

--- Fundamentals/Homework/IntroProblems2.js
+++ Fundamentals/Homework/IntroProblems2.js
@@ -10,4 +10,29 @@ function titleize(arr, cb) {
     }
 }
 
-titleize(["Mary", "Brian", "Leo"], printCallback)
+function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = [];
+    tricks.forEach(element => this.tricks.push(element))
+
+    // Below is NOT recommended to do inside of the constructor as this takes up a lot of memory
+    // elephant.prototype.trumpet = function() {
+    //     console.log(`${this.name} the elephant goes hog mild`);
+    // }
+    
+}
+
+Elephant.prototype.trumpet = function() {
+    console.log(`${this.name} the elephant trumpets!`);
+}
+
+Elephant.prototype.grow = function() {
+    this.height += 12;
+}
+
+Elephant.prototype.addTrick = function(trick) {
+    this.tricks.push(trick)
+}
+
+var q = new Elephant("avon", 12, ["Dancing", "Smokin", "Spittin"])
