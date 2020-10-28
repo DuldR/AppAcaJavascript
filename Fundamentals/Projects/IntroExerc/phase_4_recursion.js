46aff5743a0045e24c4bf396f6cca142dc252b61
Garrett <gnjulak@gmail.com>
2020-10-28 16:44:21 -0500

    Need to complete bsearch.

--- Fundamentals/Projects/IntroExerc/phase_4_recursion.js
+++ Fundamentals/Projects/IntroExerc/phase_4_recursion.js
@@ -57,3 +57,22 @@ function exponent2(b, n) {
 
     return finalSum;
 }
+
+
+function fib(n) {
+
+    if (n === 1) {
+        return [0, 1];
+    } else if (n === 2) {
+        return [0, 1, 1];
+    } else {
+        arr = fib(n - 1);
+        arr.push(arr[arr.length-1] + arr[arr.length-2]);
+    }
+
+    return arr;
+}
+
+function bsearch(arr, target) {
+    
+}
