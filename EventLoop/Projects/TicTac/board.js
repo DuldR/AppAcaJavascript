c778e97aebab6685882fbcf3544ddab9248fcfc7
Garrett <gnjulaka@gmail.com>
2020-12-11 13:54:56 -0600

    Hor and vert checks are working. need to complete diagonal check

--- EventLoop/Projects/TicTac/board.js
+++ EventLoop/Projects/TicTac/board.js
@@ -1,7 +1,7 @@
 class Board {
     constructor () {
 
-        this.board = [['o'],['x','x', 'x'],[]];
+        this.board = [['x'],['o','o', 'x'],['x','o','o']];
 
     }
 
@@ -9,7 +9,7 @@ class Board {
 
         // This verifies a horizontal win.
         let horCheck = (checkArr) => {
-            
+
             return (JSON.stringify(checkArr) === JSON.stringify([`${player}`, `${player}`, `${player}`]));
         }
 
@@ -17,7 +17,26 @@ class Board {
 
     }
 
-    verticalCheck() {
+    verticalCheck(player) {
+
+        // This verifies a vertical win.
+        let checkVal = JSON.stringify([`${player}`, `${player}`, `${player}`]);
+        let boolVal = false;
+
+        for (let i = 0; i < 3; i++) {
+
+            let emptyArr = [];
+
+            for (let j = 0; j < 3; j++) {
+                emptyArr.push(this.board[j][i]);
+            }
+
+            if (JSON.stringify(emptyArr) === checkVal) {
+                boolVal = true;
+            }
+        }
+
+        return boolVal;
 
     }
 
@@ -41,6 +60,7 @@ class Board {
 }
 
 b = new Board();
-console.log(b.horizontalCheck('x'));
+// console.log(b.horizontalCheck('x'));
+console.log(b.verticalCheck('x'));
 
 // export { Board }
--- EventLoop/Projects/TicTac/scratch.js
+++ EventLoop/Projects/TicTac/scratch.js
@@ -1,3 +1,24 @@
-let string = (checkArr) => {
-    return (JSON.stringify(checkArr) === JSON.stringify(['x', 'x', 'x']));
-}
+function test() {
+    a = [['o', 'x'], ['o', 'x', 'x'], ['o', 'o']];
+
+    let checkVal = JSON.stringify([`o`, `o`, `o`]);
+    let boolVal = false
+
+    for (let i = 0; i < 3; i++) {
+        let emptyArr = [];
+        for (let j = 0; j < 3; j++) {
+            emptyArr.push(a[j][i])
+        }
+        // console.log(i);
+        // console.log(a[i]);
+        // console.log(a[i][i])
+        if (JSON.stringify(emptyArr) === checkVal) {
+            boolVal = true;
+        }
+    }
+
+    return boolVal;
+
+ }
+
+ console.log(test());
