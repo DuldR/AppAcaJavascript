d930a2c050a0f2fd65450f32bdd9d6cf5392a4f3
Garrett <gnjulaka@gmail.com>
2020-12-23 20:19:04 -0600

    Moving object and asteroid appear

--- OOBJavascript/Projects/asteroids/src/index.js
+++ OOBJavascript/Projects/asteroids/src/index.js
@@ -1,9 +1,7 @@
-// console.log("Webpack is working");
+console.log("Webpack is working");
 const MovingObject = require('../src/moving_object.js');
 const Asteroid = require('../src/asteroid.js');
 
-console.log(MovingObject);
-console.log(Asteroid);
 
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -13,32 +11,19 @@ document.addEventListener("DOMContentLoaded", function(){
     const canvas = document.getElementById('game-canvas');
     const ctx = canvas.getContext('2d');
     window.MovingObject = MovingObject;
-    window.Asteroid = Asteroid;
     const b = new MovingObject({
         pos: [100, 100],
         vel: [10, 10],
-        radius: 100,
+        rad: 100,
         color: "red"
     });
 
     const a = new Asteroid({
-        pos: [200, 200]
+        pos: [300, 300]
     });
 
     b.draw(ctx);
     a.draw(ctx);
-
-    
-
-    
-    // Need to draw moving object here.
-    // How to fill in the window? May need webpack
-    // ctx.beginPath();
-    // ctx.arc(100, 100, 100, 0, 2 * Math.PI, false);
-    // ctx.fillStyle = 'red';
-    // ctx.fill();
-
-
     
 
 });
